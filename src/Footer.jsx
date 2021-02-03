24b7fcddf1b2c8686aec35a4466a52bdb25039ad
noxsios <harryrndzz@gmail.com>
2021-02-04 00:09:10 +0100

    Update footer

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -3,7 +3,7 @@ const Footer = () => {
   return (
     <div style={{ marginTop: "1rem", padding: "1rem", position: "fixed", bottom: 0, left: 0, width: "100%" }}>
       <Grid container spacing={1} direction="row" justify="center" alignItems="center" alignContent="center" wrap="nowrap">
-        SrA Harry Randazzo - aka Razzle-Dazzle ✨
+        <small style={{ userSelect: "none" }}>SrA Harry Randazzo - aka Razzle-Dazzle ✨</small>
       </Grid>
     </div>
   );
